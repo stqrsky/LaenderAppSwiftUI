5ac79be5ac7acd26b076b464d1944bc71ae2bc6e
Starsky <starsky.m@live.com>
2022-05-10 19:11:36 +0200

    add flagImages, List with HStack, add modifiers

--- LaenderAppSwiftUI/ContentView.swift
+++ LaenderAppSwiftUI/ContentView.swift
@@ -11,9 +11,29 @@ struct ContentView: View {
     let countries: [Country] = Bundle.main.decode(fileName: "data.json")
     
     var body: some View {
-        List(countries) { country in 
-            Text(country.countryName)
+        NavigationView {
+            List(countries) { country in
+                HStack {
+                    Image(country.imageName)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 50, height: 50)
+                        .clipShape(Circle())
+                        .overlay(
+                            Circle()
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                    
+                    Text(country.countryName)
+                        .font(.headline)
+                }
+                .padding([.top, .bottom], 10)
+                
+            }
+            .navigationBarTitle("Länder")
         }
+        
+       
     }
 }
 
