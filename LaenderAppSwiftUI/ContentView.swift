772649aa636015058bd447af9179758da71eb532
Starsky <starsky.m@live.com>
2022-05-10 16:55:52 +0200

    Initial Commit

---
+++ LaenderAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LaenderAppSwiftUI
+//
+//  Created by star on 10.05.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LaenderAppSwiftUI/LaenderAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaenderAppSwiftUIApp.swift
+//  LaenderAppSwiftUI
+//
+//  Created by star on 10.05.22.
+//
+
+import SwiftUI
+
+@main
+struct LaenderAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
